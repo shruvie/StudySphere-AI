f2160296a9add2dcb305acb9380f95de4af56d59
Vaibhav Sharma <vaibhavsharma6060@gmail.com>
2025-12-10 20:19:34 +0530

    Initial commit from VS Code

---
+++ src/pages/Index.tsx
@@ -0,0 +1,29 @@
+import Navigation from "@/components/Navigation";
+import HeroSection from "@/components/HeroSection";
+import ProblemSection from "@/components/ProblemSection";
+import SolutionSection from "@/components/SolutionSection";
+import FeaturesSection from "@/components/FeaturesSection";
+import HowItWorksSection from "@/components/HowItWorksSection";
+import TechStackSection from "@/components/TechStackSection";
+import DemoSection from "@/components/DemoSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <main className="min-h-screen bg-background">
+      <Navigation />
+      <HeroSection />
+      <ProblemSection />
+      <SolutionSection />
+      <FeaturesSection />
+      <HowItWorksSection />
+      <TechStackSection />
+      <DemoSection />
+      <ContactSection />
+      <Footer />
+    </main>
+  );
+};
+
+export default Index;
